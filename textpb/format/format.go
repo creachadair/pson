5f76e9098e7b5ec8e0181f3aaeef9fbb6a31c781
M. J. Fromberger <michael.j.fromberger@gmail.com>
2016-08-10 22:20:43 -0700

    Add "format" package to render text-format messages.
    This package supports the inverse of parsing, emitting the protobuf text format
    based on the AST. The formatter does not validate the values, so a message that
    is constructed programmatically may result in unparseable output; however, any
    message that was parsed successfully by textpb.Parse should render to a string
    that is re-parseable.

---
+++ textpb/format/format.go
@@ -0,0 +1,102 @@
+// Package format renders textpb.Message values to protobuf text format.
+package format
+
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"bitbucket.org/creachadair/pson/textpb"
+)
+
+var (
+	// Curly
+	left  = map[bool]string{false: "<", true: "{"}
+	right = map[bool]string{false: ">", true: "}"}
+	// Compact
+	space = map[bool]string{false: " ", true: ""}
+	next  = map[bool]string{false: "\n", true: " "}
+	end   = map[bool]string{false: "\n", true: ""}
+)
+
+type Config struct {
+	Compact bool   // If true, omit vertical whitespace.
+	Curly   bool   // If true, use {} for grouping rather than <>.
+	Indent  string // Use this string for each level of indentation.
+}
+
+func (c Config) Text(w io.Writer, msg textpb.Message) error { return c.textMessage(w, msg, 0) }
+
+func (c Config) textMessage(w io.Writer, msg textpb.Message, level int) error {
+	for i, field := range msg {
+		if err := c.textField(w, field, level, i < len(msg)-1); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c Config) textField(w io.Writer, field *textpb.Field, level int, sep bool) error {
+	if len(field.Values) == 0 { // empty: treat as empty repeated field
+		return fp(w, c.indent(level), field.Name, c.space(), c.left(), c.right())
+	}
+	for i, value := range field.Values {
+		if err := c.textValue(w, field.Name, value, level, i < len(field.Values)-1); err != nil {
+			return err
+		}
+	}
+	return c.next(w, sep)
+}
+
+func (c Config) textValue(w io.Writer, name string, value *textpb.Value, level int, sep bool) error {
+	if err := fp(w, c.indent(level), name); err != nil {
+		return err
+	}
+	if value.Msg == nil {
+		return fp(w, ":", c.space(), tokenText(value))
+	}
+	if len(value.Msg) == 0 {
+		return fp(w, " ", c.left(), c.right())
+	} else if err := fp(w, " ", c.left(), c.first()); err != nil {
+		return err
+	} else if err := c.textMessage(w, value.Msg, level+1); err != nil {
+		return err
+	} else if err := fp(w, c.last(), c.indent(level), c.right()); err != nil {
+		return err
+	}
+	return c.next(w, sep)
+}
+
+func (c Config) indent(level int) string {
+	if c.Compact {
+		return ""
+	} else if c.Indent == "" {
+		return strings.Repeat("  ", level)
+	}
+	return strings.Repeat(c.Indent, level)
+}
+
+func tokenText(v *textpb.Value) string {
+	if v.Type == textpb.String {
+		return `"` + strings.Replace(v.Text, `"`, `\"`, -1) + `"`
+	}
+	return v.Text
+}
+
+func (c Config) left() string  { return left[c.Curly] }
+func (c Config) right() string { return right[c.Curly] }
+func (c Config) space() string { return space[c.Compact] }
+func (c Config) first() string { return end[c.Compact] }
+func (c Config) last() string  { return end[c.Compact] }
+
+func (c Config) next(w io.Writer, want bool) error {
+	if want {
+		return fp(w, next[c.Compact])
+	}
+	return nil
+}
+
+func fp(w io.Writer, args ...interface{}) error {
+	_, err := fmt.Fprint(w, args...)
+	return err
+}
---
+++ textpb/format/format_test.go
@@ -0,0 +1,62 @@
+package format
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+
+	"bitbucket.org/creachadair/pson/textpb"
+)
+
+var configs = []Config{
+	{false, false, "@"},
+	{false, true, "@"},
+	{true, false, "@"},
+	{true, true, "@"},
+}
+
+var sub = strings.NewReplacer("*", "\n")
+var a2c = strings.NewReplacer("<", "{", ">", "}")
+
+func ans(verbose, compact string) []string {
+	v := sub.Replace(verbose)
+	c := sub.Replace(compact)
+	return []string{v, a2c.Replace(v), c, a2c.Replace(c)}
+}
+
+func TestTextFormatting(t *testing.T) {
+	tests := []struct {
+		input  string
+		answer []string
+	}{
+		{"", ans("", "")},
+		{"a<\n>", ans("a <>", "a <>")},
+		{"a: 1", ans("a: 1", "a:1")},
+		{"a:\n\t\"foo\"\n", ans(`a: "foo"`, `a:"foo"`)},
+		{"a<n:1>", ans("a <*@n: 1*>", "a <n:1>")},
+		{`a<n:1 s:"foo">a<n:2 s:"bar">`,
+			ans(`a <*@n: 1*@s: "foo"*>*a <*@n: 2*@s: "bar"*>`, `a <n:1 s:"foo"> a <n:2 s:"bar">`)},
+		{`a { b {} } a {} c:<>`,
+			ans(`a <*@b <>*>*a <>*c <>`, `a <b <>> a <> c <>`)},
+		{`a{b{c:1 c:2 c:3}d{e{f:0x3f}}}`,
+			ans(`a <*@b <*@@c: 1*@@c: 2*@@c: 3*@>*@d <*@@e <*@@@f: 0x3f*@@>*@>*>`, `a <b <c:1 c:2 c:3> d <e <f:0x3f>>>`)},
+	}
+	for _, test := range tests {
+		msg, err := textpb.ParseString(test.input)
+		if err != nil {
+			t.Fatalf("[BROKEN TEST] Parsing %q failed: %v", test.input, err)
+		}
+		for i, cfg := range configs {
+			t.Logf("Config: %+v", cfg)
+			var buf bytes.Buffer
+			if err := cfg.Text(&buf, msg); err != nil {
+				t.Errorf("Text %#q: unexpected error: %v", test.input, err)
+				continue
+			}
+			got, want := buf.String(), test.answer[i]
+			if got != want {
+				t.Errorf("Text %#q: got\n«%s»\nwant\n«%s»", test.input, got, want)
+			}
+		}
+	}
+}
