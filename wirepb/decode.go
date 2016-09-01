3a3d6e99429945cbacdef676ecc44a981586cbef
M. J. Fromberger <michael.j.fromberger@gmail.com>
2016-09-01 14:54:54 -0700

    Rework the wirepb package.
    - Rename Type to wireType; only the package constants are valid.
    - Add Pack and PackValue methods to *Field.
    - Add unit tests for packing.

--- wirepb/decode.go
+++ wirepb/decode.go
@@ -26,7 +26,7 @@ func (d Decoder) Next() (*Field, error) {
 	}
 	f := &Field{
 		ID:   int(v >> 3),
-		Wire: Type(v & 7),
+		Wire: wireType(v & 7),
 	}
 
 	switch f.Wire {
@@ -35,13 +35,7 @@ func (d Decoder) Next() (*Field, error) {
 		if err != nil {
 			return nil, err
 		}
-		var buf [8]byte
-		i := 0
-		for ; w > 0; w >>= 8 {
-			buf[i] = byte(w & 255)
-			i++
-		}
-		f.Data = buf[:i]
+		f.Data = varintToData(w)
 		return f, nil
 
 	case TFixed64:
@@ -66,18 +60,16 @@ func (d Decoder) Next() (*Field, error) {
 	return f, nil
 }
 
-// A Type represents the wire type of a field key
-type Type int
+// A wireType represents the wire type of a field key
+type wireType int
 
 const (
-	TVarint     Type = 0 // varint-encoded value
-	TFixed64    Type = 1 // fixed-width 64-bit value (LSB first)
-	TDelimited  Type = 2 // length-prefixed value (varint + bytes)
-	TStartGroup Type = 3 // deprecated, unused
-	TEndGroup   Type = 4 // deprecated, unused
-	TFixed32    Type = 5 // fixed-width 32-bit value (LSB first)
-
-	lastType = TFixed32
+	TVarint     wireType = 0 // varint-encoded value
+	TFixed64    wireType = 1 // fixed-width 64-bit value (LSB first)
+	TDelimited  wireType = 2 // length-prefixed value (varint + bytes)
+	TStartGroup wireType = 3 // deprecated, unused
+	TEndGroup   wireType = 4 // deprecated, unused
+	TFixed32    wireType = 5 // fixed-width 32-bit value (LSB first)
 )
 
 // A Field represents a field read from a wire-format message.  The data in the
@@ -85,26 +77,110 @@ const (
 // is the caller's responsibility.
 type Field struct {
 	ID   int
-	Wire Type
+	Wire wireType
 	Data []byte
 }
 
 // Size reports the number of bytes needed to encode f in wire format, or 0 if
 // f cannot be encoded.
 func (f *Field) Size() int {
+	n := varintSize(uint64(f.ID) << 3)
 	switch f.Wire {
 	case TVarint:
-		return (8*len(f.Data) + 6) / 7
+		return n + (8*len(f.Data)+6)/7
 	case TFixed64:
-		return 8
+		return n + 8
 	case TDelimited:
-		vlen := 1
-		for n := uint(len(f.Data)) >> 7; n != 0; vlen++ {
-		}
-		return vlen + len(f.Data)
+		return n + varintSize(uint64(len(f.Data))) + len(f.Data)
 	case TFixed32:
-		return 4
+		return n + 4
 	default:
 		return 0
 	}
+}
+
+// Pack encodes f in wire format and appends the result to buf, allowing the
+// caller to control allocation.
+func (f *Field) Pack(buf []byte) []byte {
+	var bits [10]byte // buffer for varint encoding
+
+	// Pack the field ID.
+	key := (uint64(f.ID) << 3) | uint64(f.Wire)
+	n := binary.PutUvarint(bits[:], key)
+
+	// Pack the field value.
+	return f.PackValue(append(buf, bits[:n]...))
+}
+
+// PackValue encodes the value of f in wire format and appends the result to
+// buf, allowing the caller to control allocation. Returns nil if f cannot be
+// packed.
+func (f *Field) PackValue(buf []byte) []byte {
+	var bits [10]byte // buffer for varint encoding
+
+	// Pack the field value
+	switch f.Wire {
+	case TVarint:
+		return append(buf, dataToVarint(f.Data)...)
+
+	case TDelimited:
+		n := binary.PutUvarint(bits[:], uint64(len(f.Data)))
+		buf = append(buf, bits[:n]...)
+		return append(buf, f.Data...)
+
+	case TFixed64:
+		return appendN(buf, f.Data, 8)
+
+	case TFixed32:
+		return appendN(buf, f.Data, 4)
+
+	default:
+		return nil
+	}
+}
+
+// appendN appends up to n bytes from data to old, padding with zeroes if
+// len(data) < n, and returns the expanded slice.
+func appendN(old, data []byte, n int) []byte {
+	t := len(data)
+	if t > n {
+		t = n
+	}
+	old = append(old, data[:t]...)
+	for t < n {
+		old = append(old, 0)
+		t++
+	}
+	return old
+}
+
+// varintSize reports the number of bytes needed to encode v as a varint.
+func varintSize(v uint64) int {
+	n := 1
+	for v >>= 7; v != 0; v >>= 7 {
+		n++
+	}
+	return n
+}
+
+// varintToData converts v into bytes in packed form.
+func varintToData(v uint64) []byte {
+	var buf [8]byte
+	binary.BigEndian.PutUint64(buf[:], v)
+	for i, b := range buf {
+		if b != 0 {
+			return buf[i:]
+		}
+	}
+	return buf[:1]
+}
+
+func dataToVarint(data []byte) []byte {
+	var w uint64
+	for _, b := range data {
+		w = (w << 8) | uint64(b)
+	}
+	var bits [10]byte
+	n := binary.PutUvarint(bits[:], w)
+	return bits[:n]
 }
--- wirepb/decode_test.go
+++ wirepb/decode_test.go
@@ -8,18 +8,18 @@ import (
 )
 
 func TestDecoding(t *testing.T) {
-	//             @1  #8  ....... @2  ........@3  .   .   .   .   .   .   .   .   .   @4  ....
-	const input = "\012\010abcdefgh\021abcdefgh\030\xe1\xc4\x8d\xa3\xd6\xcc\xd9\xb3\x68\045ABCD"
+	//             @1  #8  ....... @2  ........@3  .   .   .   .   .   @4  ....
+	const input = "\012\010abcdefgh\021abcdefgh\030\xc4\x86\x89\x8a\x04\045****"
 
 	want := []struct {
 		key  int
-		wire Type
+		wire wireType
 		data string
 	}{
 		{1, TDelimited, "abcdefgh"},
 		{2, TFixed64, "abcdefgh"},
-		{3, TVarint, "abcdefgh"},
-		{4, TFixed32, "ABCD"},
+		{3, TVarint, "ABCD"},
+		{4, TFixed32, "****"},
 	}
 	dec := NewDecoder(strings.NewReader(input))
 	for i, test := range want {
@@ -31,6 +31,45 @@ func TestDecoding(t *testing.T) {
 		want := &Field{test.key, test.wire, []byte(test.data)}
 		if diff := pretty.Compare(got, want); diff != "" {
 			t.Errorf("Record %d result differs from expected (-got, +want)\n%s", i, diff)
+		}
+	}
+}
+
+func decode1(s string) *Field {
+	f, err := NewDecoder(strings.NewReader(s)).Next()
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func TestPacking(t *testing.T) {
+	tests := []struct {
+		id         int
+		wire       wireType
+		data, want string
+	}{
+		{1, TFixed32, "abcd", "\015abcd"},
+		{2, TFixed64, "abcdefgh", "\021abcdefgh"},
+		{3, TDelimited, "apple pie and cake", "\032\022apple pie and cake"},
+		{4, TVarint, "ABCD", " \xc4\x86\x89\x8a\x04"},
+		{47, TFixed32, "0123", "\xfd\x020123"},
+	}
+	for _, test := range tests {
+		input := &Field{ID: test.id, Wire: test.wire, Data: []byte(test.data)}
+		got := string(input.Pack(nil))
+		n := input.Size()
+
+		if len(got) != n {
+			t.Errorf("Pack %+v: got length %d, want %d", input, len(got), n)
+		}
+		if got != test.want {
+			t.Errorf("Pack %+v: got %#v, want %#v", input, []byte(got), []byte(test.want))
+		}
+
+		rt := decode1(got)
+		if diff := pretty.Compare(rt, input); diff != "" {
+			t.Errorf("Pack result did not round-trip (-got, +want)\n%s", diff)
 		}
 	}
 }
